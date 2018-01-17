9a508a2445b0b519d75a08026756b6ea47fd91a0
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-01-17 09:22:43 -0500

    a

---
+++ Lab 3.1 In-Place Sorts/src/ArrayMethods1.java
@@ -0,0 +1,22 @@
+
+public class ArrayMethods1 {
+	
+	public static void insertionSort(int[] list1) {
+		
+	}
+	public static void selectionSort(double [] list1) {
+		
+	}
+	public static void bubbleSort(String [] list1) {
+		
+	}
+	public static void swapInt(int[] list1, int x, int y) {
+		
+	}
+	public static void swapDouble(double[] list1, int x, int y) {
+		
+	}
+	public static void swapInt(String[] list1, int x, int y) {
+	
+	}
+}
