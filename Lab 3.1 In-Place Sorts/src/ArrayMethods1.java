5ff9d2ec1c7d336756609448b91f26401778d1b3
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-18 09:23:33 -0500

    hi

--- Lab 3.1 In-Place Sorts/src/ArrayMethods1.java
+++ Lab 3.1 In-Place Sorts/src/ArrayMethods1.java
@@ -1,22 +1,54 @@
 
 public class ArrayMethods1 {
 	
+	public static void main(String args[]) {
+		int[] list1 = {1, 2, 3, 4};
+		swapInt(list1, 2, 3);
+	}
+	
+	/*Insertion takes each element from the array,
+	 *  and adds it to the front of the array in the
+	 *   correct order. We need to keep track of where
+	 *    we are inserting elements as we sort. */
+	
 	public static void insertionSort(int[] list1) {
 		
 	}
+	
+	/*Selection sort finds the lowest element and
+	 *  moves it to the front of the array. Then 
+	 *  searches the rest of the array for the next
+	 *   element, and repeats.*/
+	
 	public static void selectionSort(double [] list1) {
 		
 	}
+	
+	/* Bubble sort iterates through the list, swapping
+	 *  any out of order elements. We keep iterating until
+	 *  no swaps are required. Then the array is sorted! */
+	
 	public static void bubbleSort(String [] list1) {
 		
 	}
 	public static void swapInt(int[] list1, int x, int y) {
+		int z = x;
+		// x = 2, y = 3, z = 2
+		list1[x] = list1[y];
+		list1[y] = z;
 		
+		for (int i = 0; i < list1.length; i++) {
+			System.out.println(list1[i]);
+		}
 	}
 	public static void swapDouble(double[] list1, int x, int y) {
-		
+		int z = x;
+		list1[x] = list1[y];
+		list1[y] = list1[z];
 	}
-	public static void swapInt(String[] list1, int x, int y) {
-	
+	public static void swapString(String[] list1, int x, int y) {
+		int z = x;
+		list1[x] = list1[y];
+		list1[y] = list1[z];
 	}
 }
