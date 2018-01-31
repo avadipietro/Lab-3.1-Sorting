ea2a0736bffb483be3b30fa19383dad626303e80
Student <Student@10.148.32.46>
2018-01-31 13:59:01 -0500

    bubbleSort

--- Lab 3.1 In-Place Sorts/src/ArrayMethods1.java
+++ Lab 3.1 In-Place Sorts/src/ArrayMethods1.java
@@ -3,7 +3,11 @@ public class ArrayMethods1 {
 	
 	public static void main(String args[]) {
 		int[] list1 = {2, 1, 4, 3};
+		double[] list2 = {2.0, 1.0, 4.0, 3.0};
+		String[] list3 = {"hi", "jo", "shha", "ava"};
 		insertionSort(list1);
+		selectionSort(list2);
+		bubbleSort(list3);
 	}
 	
 	/*Insertion takes each element from the array,
@@ -40,22 +44,41 @@ public class ArrayMethods1 {
 	 *  no swaps are required. Then the array is sorted! */
 	
 	public static void bubbleSort(String [] list1) {
-		
+		boolean swapped = false;
+		while (swapped == false) {
+			for (int x = 0; x <= list1.length - 1; x += 2) {
+				if (list1[x].compareTo(list1[x + 1]) < 0) {
+					swapString(list1, x, x + 1);
+				}
+			}
+			for (int y = 0; y <= list1.length - 2; y++) {
+				int count = 0;
+				if (list1[y].compareTo(list1[y + 1]) < 0) {
+					count++;
+				}
+				if (count == list1.length) {
+					swapped = false;
+				}
+			}
+		}
+		for (String l : list1) {
+			System.out.println(l);
+		}
 	}
 	public static void swapInt(int[] list1, int x, int y) {
-		int z = x;
+		int z = list1[x];
 		// x = 2, y = 3, z = 2
 		list1[x] = list1[y];
 		list1[y] = z;
 	}
 	public static void swapDouble(double[] list1, int x, int y) {
-		int z = x;
+		double z = list1[x];
 		list1[x] = list1[y];
-		list1[y] = list1[z];
+		list1[y] = z;
 	}
 	public static void swapString(String[] list1, int x, int y) {
-		int z = x;
+		String z = list1[x];
 		list1[x] = list1[y];
-		list1[y] = list1[z];
+		list1[y] = z;
 	}
 }
